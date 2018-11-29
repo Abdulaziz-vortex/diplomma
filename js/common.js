d4ebaccb4876489d82439f208e6f0276b5638419
optimist23 <r.opt1m1st23@gmail.com>
2018-11-29 22:01:10 +0500

    исправление ошибки вывода модального окна

---
+++ js/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	$('#button-callback').on('click', function() {
+		$('#modal-callback').toggleClass('modal_visible')
+	});
+	$('.modal__close').on('click', function() {
+		$('#modal-callback').toggleClass('modal_visible')
+	});
+});
